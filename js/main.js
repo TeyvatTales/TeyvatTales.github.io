fda21409636bb1bc13a20c3fd6001847ae9eb158
Kenny Lazaro <54491750+k-lazaro@users.noreply.github.com>
2022-04-14 03:38:05 -0700

    Added timer to HoYoLab Login

--- js/main.js
+++ js/main.js
@@ -48,3 +48,35 @@
             currentBoss.src = "images/Enemy_Andrius_Icon.png";
         }
     }
+
+    function hoyoLabTimer() {
+        const now = new Date();
+        var hours = now.getUTCHours();
+        var minutes = now.getUTCMinutes();
+        var seconds = now.getUTCSeconds();
+
+        // Change to UTC+8
+        if (hours + 8 >= 24) {
+            now.setUTCHours(24);
+            now.setUTCHours(hours + 8 - 24);
+        }
+        else {
+            now.setUTCHours(hours + 8);
+        }
+        
+        // End date at midnight
+        const end = new Date(now);
+        end.setUTCDate(end.getUTCDate() + 1);
+        end.setUTCHours(0, 0, 0, 0);
+
+        const timeLeft = end.getTime() - now.getTime();
+
+        var hrs = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var mins = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        var s = Math.floor((timeLeft % (1000 * 60)) / 1000);
+        document.getElementById("checkInHrs").innerHTML = hrs;
+        document.getElementById("checkInMins").innerHTML = mins;
+        document.getElementById("checkInS").innerHTML = s;
+    }
+
+    var hoyoFunction = setInterval(hoyoLabTimer, 1000);
