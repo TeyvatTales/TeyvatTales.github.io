a8fcc99c036e4cabebbeceb1a2e2136077603269
kevinvietnguyen <54491766+kevinvietnguyen@users.noreply.github.com>
2022-05-08 19:32:05 -0700

    Other Timers
    Parametric, Banner, Weekly Bosses timer

--- js/main.js
+++ js/main.js
@@ -120,6 +120,7 @@
     }
 
     var spiralFunction = setInterval(spiralTimer, 1000);
+
     var resin = 0;
 
     function resinTimer() {
@@ -145,4 +146,104 @@
     }
 
     var resinFunction = setInterval(resinTimer, 1000);
+
+
+    var parametricTimeLeft = 597600000;
+
+    function parametricTimerCalculate() {
+        const now = new Date();
+        var hours = now.getHours();
+        var minutes = now.getMinutes();
+        var seconds = now.getSeconds();
+
+        const end = new Date(now);
+        end.setDate(end.getDate() + 6);
+        end.setHours(end.getHours() + 22);
+        parametricTimeLeft = end.getTime() - now.getTime();
+    }
+
+    function parametricTimer() {
+        var days = Math.floor(parametricTimeLeft / (1000 * 60 * 60 * 24));
+        var hrs = Math.floor((parametricTimeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var mins = Math.floor((parametricTimeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        var s = Math.floor((parametricTimeLeft % (1000 * 60)) / 1000);
+
+        document.getElementById("parametricDays").innerHTML = days;
+        document.getElementById("parametricHrs").innerHTML = hrs;
+        document.getElementById("parametricMins").innerHTML = mins;
+        document.getElementById("parametricS").innerHTML = s;
+
+        parametricTimeLeft = parametricTimeLeft - 1000;
+    }
+
+    var parametricFunction = setInterval(parametricTimer, 1000);
+
+    function bossTimer() {
+        const now = new Date();
+        var hours = now.getUTCHours();
+        var minutes = now.getUTCMinutes();
+        var seconds = now.getUTCSeconds();
+
+        // Change to UTC-5 (North America)
+        if (hours - 5 < 0) {
+            now.setUTCHours(-1);
+            now.setUTCHours(hours - 5 + 24);
+        }
+        else {
+            now.setUTCHours(hours - 5);
+        }
+        
+        const end = new Date(now);
+        end.setDate(now.getDate() + (((1 + 7 - now.getDay()) % 7) || 7));
+        end.setUTCHours(4, 0, 0, 0);
+
+        const timeLeft = end.getTime() - now.getTime();
+
+        var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        var hrs = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var mins = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        var s = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+        document.getElementById("bossDays").innerHTML = days;
+        document.getElementById("bossHrs").innerHTML = hrs;
+        document.getElementById("bossMins").innerHTML = mins;
+        document.getElementById("bossS").innerHTML = s;
+    }
+
+    var bossFunction = setInterval(bossTimer, 1000);
+
+    var currentBannerStartDate = new Date(2022,3,19,0,0,0,0);
+    function bannerTimer() {
+        const now = new Date();
+        var hours = now.getUTCHours();
+        var minutes = now.getUTCMinutes();
+        var seconds = now.getUTCSeconds();
+
+        // Change to UTC-5 (North America)
+        if (hours - 5 < 0) {
+            now.setUTCHours(-1);
+            now.setUTCHours(hours - 5 + 24);
+        }
+        else {
+            now.setUTCHours(hours - 5);
+        }
+        
+        const end = new Date(currentBannerStartDate);
+        end.setUTCDate(end.getUTCDate() + 21);
+        end.setUTCHours(4, 0, 0, 0);
+
+        const timeLeft = end.getTime() - now.getTime();
+
+        var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        var hrs = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var mins = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        var s = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+        document.getElementById("bannerDays").innerHTML = days;
+        document.getElementById("bannerHrs").innerHTML = hrs;
+        document.getElementById("bannerMins").innerHTML = mins;
+        document.getElementById("bannerS").innerHTML = s;
+    }
+
+    var bannerFunction = setInterval(bannerTimer, 1000);
 
